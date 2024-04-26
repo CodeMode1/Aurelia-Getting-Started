3d306cc24436952565de863dc52f51a9ca01fa35
CodeMode1 <daphnedc88@gmail.com>
2024-04-26 12:17:08 -0400

    todos test

--- todo/src/app.ts
+++ todo/src/app.ts
@@ -1,3 +1,33 @@
+interface ITodo {
+  description: string;
+  done: boolean;
+}
+
 export class App {
-  public message = 'Hello World!';
+  heading: string;
+    todos: ITodo[];
+    todoDescription: string;
+  
+    constructor() {
+      this.heading = 'Todos';
+      this.todos = [];
+      this.todoDescription = '';
+    }
+  
+    addTodo(): void {
+      if (this.todoDescription) {
+        this.todos.push({
+          description: this.todoDescription,
+          done: false
+        });
+        this.todoDescription = '';
+      }
+    }
+  
+    removeTodo(todo: ITodo): void {
+      const index = this.todos.indexOf(todo);
+      if (index !== -1) {
+        this.todos.splice(index, 1);
+      }
+    }
 }
